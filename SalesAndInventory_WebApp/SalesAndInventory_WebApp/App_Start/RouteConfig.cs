ad11837e1a6f0cd5e29c2b90d2b1ccf71ab3b591
priyanka <priyashinde671@gmail.com>
2021-05-22 10:55:46 +0530

    Dashboard

--- SalesAndInventory_WebApp/SalesAndInventory_WebApp/App_Start/RouteConfig.cs
+++ SalesAndInventory_WebApp/SalesAndInventory_WebApp/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace SalesAndInventory_WebApp
+namespace MVCwebInvenotry
 {
     public class RouteConfig
     {
--- SalesAndInventory_WebApp/SalesAndInventory_WebApp/Properties/AssemblyInfo.cs
+++ SalesAndInventory_WebApp/SalesAndInventory_WebApp/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SalesAndInventory_WebApp")]
+[assembly: AssemblyTitle("MVCwebInvenotry")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SalesAndInventory_WebApp")]
+[assembly: AssemblyProduct("MVCwebInvenotry")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c9d1a506-5e1b-4d56-b8dc-6cf1f90d7580")]
+[assembly: Guid("b6988ce6-742c-4730-957c-466bfa363b99")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
